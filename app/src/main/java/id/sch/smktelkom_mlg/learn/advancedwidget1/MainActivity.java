524f1ec929a2094b768f4bf5b19091fe6b32b927
Bryan <bryan_marsono24rpl@student.smktelkom-mlg.sch.id>
2017-02-05 22:45:43 +0800

    Widget Programming Part 1

--- app/src/main/java/id/sch/smktelkom_mlg/learn/advancedwidget1/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/advancedwidget1/MainActivity.java
@@ -1,13 +1,38 @@
 package id.sch.smktelkom_mlg.learn.advancedwidget1;
 
+import android.renderscript.ScriptGroup;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    Spinner spJumlah;
+    LinearLayout llAnak;
+    TextView tvHasil;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        spJumlah = (Spinner) findViewById(R.id.spinnerJumlahAnak);
+        Integer [] arJumlah = new Integer[10];
+        for (int i = 0; i < 10; i++)
+        {
+            arJumlah[i]=i+1;
+        }
+        ArrayAdapter adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_item, arJumlah);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spJumlah.setAdapter(adapter);
